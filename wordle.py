2eaa2ffb8a6df15d05e60b71079cc2b30a1172b3
tysalim <tylersamuellim@gmail.com>
2024-06-18 18:49:34 +0800

    Created words textfile and skeleton for main.py

--- wordle.py
+++ wordle.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+import time
+import random
+
+
+# TODO: create globals
+
+# takes user input, repeats through each round calling the checker function
+def main():
+    print("Hello World!")
+
+# TODO: create word-select function
+
+# TODO: create checker function
+
+
+
+if __name__ == "__main__":
+    main()
